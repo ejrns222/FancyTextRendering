744611d0851c5bfe1931a2b91001d6cd12744360
ejrns222 <62171230+ejrns222@users.noreply.github.com>
2026-05-31 20:55:52 +0900

    Refactor MarkdownRenderer to support SourceMode

--- Assets/FancyTextRendering/Scripts/MarkdownRenderer.cs
+++ Assets/FancyTextRendering/Scripts/MarkdownRenderer.cs
@@ -1,4 +1,4 @@
-﻿using TMPro;
+using TMPro;
 using UnityEngine;
 
 namespace LogicUI.FancyTextRendering
@@ -6,16 +6,50 @@ namespace LogicUI.FancyTextRendering
     [RequireComponent(typeof(TMP_Text))]
     public class MarkdownRenderer : MonoBehaviour
     {
+        public enum SourceMode
+        {
+            DirectInput,
+            TextAsset
+        }
+
+        [SerializeField]
+        SourceMode _SourceMode = SourceMode.DirectInput;
+
         [SerializeField]
         [TextArea(minLines: 10, maxLines: 50)]
         string _Source;
+
+        [SerializeField]
+        TextAsset _SourceAsset;
+
+        public SourceMode InputMode
+        {
+            get => _SourceMode;
+            set
+            {
+                _SourceMode = value;
+                RenderText();
+            }
+        }
 
         public string Source
         {
             get => _Source;
             set
             {
-                _Source = value;
+                _Source = value ?? string.Empty;
+                _SourceMode = SourceMode.DirectInput;
+                RenderText();
+            }
+        }
+
+        public TextAsset SourceAsset
+        {
+            get => _SourceAsset;
+            set
+            {
+                _SourceAsset = value;
+                _SourceMode = SourceMode.TextAsset;
                 RenderText();
             }
         }
@@ -37,12 +71,27 @@ namespace LogicUI.FancyTextRendering
             RenderText();
         }
 
+        private void Awake()
+        {
+            RenderText();
+        }
+
 
         public MarkdownRenderingSettings RenderSettings = MarkdownRenderingSettings.Default;
 
         private void RenderText()
         {
-            Markdown.RenderToTextMesh(Source, TextMesh, RenderSettings);
+            Markdown.RenderToTextMesh(GetResolvedSource(), TextMesh, RenderSettings);
+        }
+
+        private string GetResolvedSource()
+        {
+            if (_SourceMode == SourceMode.TextAsset)
+            {
+                return _SourceAsset != null ? _SourceAsset.text : string.Empty;
+            }
+
+            return _Source ?? string.Empty;
         }
     }
 }
